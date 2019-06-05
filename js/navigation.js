83e613357237fbb2212a4cb359efde5188ba80e9
Tanishq Manuja <tanishqmanuja@users.noreply.github.com>
2019-06-05 20:39:18 +0530

    Added navBtn Animation

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,24 @@
-var body = document.body;
-var nav = document.querySelector('.nav');
-var navBtn = document.querySelector('#nav-btn');
+const body = document.body;
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('#nav-btn');
+const line1 = document.querySelector('#line1');
+const line2 = document.querySelector('#line2');
+const line3 = document.querySelector('#line3');
+
+const navBtnAnimation = new TimelineMax({paused:true,reversed:true});
+
+navBtnAnimation
+  .to(line1,.7,{delay:.5,y: 14.5,ease: Power2.easeOut},"join")
+  .to(line3,.7,{delay:.5,y: 14.5,ease: Power2.easeOut},"join")
+  .to(line2,0,{scaleX: 0,transformOrigin: "20 20"})
+  .to(line1,.6,{rotation: 45,transformOrigin: "20 0",ease: Power2.easeOut},"cross")
+  .to(line3,.6,{rotation: -45,transformOrigin: "20 0",ease: Power2.easeOut},"cross");
+
 
 // For Toggling
 navBtn.addEventListener('click',_=>{
   nav.classList.toggle('nav_open');
+  navBtnAnimation.reversed() ? navBtnAnimation.play() : navBtnAnimation.reverse();
 });
 
 //For disabling Scroll
