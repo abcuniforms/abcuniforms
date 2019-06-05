f6586ec3df420075bfba153256637a9d2a141816
Tanishq Manuja <tanishqmanuja@users.noreply.github.com>
2019-06-05 22:03:21 +0530

    New Animations

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,23 @@
+// SECTION HOME
+var logoTL = new TimelineMax({onUpdate:updatePercentage});
+const logoC = new ScrollMagic.Controller();
 
+logoTL
+  .from(".aboutus_text",.5,{y:15, opacity:.7})
+  .from(".sec-aboutus div",.5,{y:10,width:40, opacity:.8})
+  .from(".sec-aboutus p",.5,{y:10, opacity:.7});
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".sec-logo",
+  triggerHook: "onLeave",
+  duration: "10%"
+})
+  //.setPin(".sec-logo")
+  .setTween(logoTL)
+  .addTo(logoC);
+
+function updatePercentage(){
+  logoTL.progress();
+}
 
 
