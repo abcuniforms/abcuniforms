85e18890dd429fe89ac4087fe06d7f3595688884
Tanishq Manuja <tanishqmanuja@users.noreply.github.com>
2019-06-06 11:50:49 +0530

    fixes

--- js/animation.js
+++ js/animation.js
@@ -1,23 +1,45 @@
-// SECTION HOME
-var logoTL = new TimelineMax({onUpdate:updatePercentage});
-const logoC = new ScrollMagic.Controller();
+Element.prototype.setAttributes = function (attrs) {
+  for (var idx in attrs) {
+      if ((idx === 'styles' || idx === 'style') && typeof attrs[idx] === 'object') {
+          for (var prop in attrs[idx]){this.style[prop] = attrs[idx][prop];}
+      } else if (idx === 'html') {
+          this.innerHTML = attrs[idx];
+      } else {
+          this.setAttribute(idx, attrs[idx]);
+      }
+  }
+};
 
-logoTL
-  .from(".aboutus_text",.5,{y:15, opacity:.7})
-  .from(".sec-aboutus div",.5,{y:10,width:40, opacity:.8})
-  .from(".sec-aboutus p",.5,{y:10, opacity:.7});
 
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".sec-logo",
-  triggerHook: "onLeave",
-  duration: "100%"
+// SECTION ABOUT US
+
+
+// SECTION OUR PRODUCTS
+document.querySelector('.sec-ourproducts').setAttributes({
+  "data-aos":"fade-up",
+  "data-aos-ease":"ease-in",
+  "data-aos-duration":"1000",
+  "data-aos-once":"false",
+  "data-aos-anchor":".sec-aboutus h1",
+  "data-aos-anchor-placement":"top-top",
 })
-  //.setPin(".sec-logo")
-  .setTween(logoTL)
-  .addTo(logoC);
 
-function updatePercentage(){
-  logoTL.progress();
-}
+// SECTION FIND US
+document.querySelector('.sec-findus').setAttributes({
+  "data-aos":"fade-up",
+  "data-aos-ease":"ease-in",
+  "data-aos-duration":"1000",
+  "data-aos-once":"false",
+  "data-aos-anchor":".sec-ourproducts h1",
+  "data-aos-anchor-placement":"top-top",
+})
 
-
+// SECTION CONTACT US
+document.querySelector('.sec-contactus').setAttributes({
+  "data-aos":"fade-up",
+  "data-aos-ease":"ease-in",
+  "data-aos-duration":"1000",
+  "data-aos-once":"false",
+  "data-aos-anchor":".sec-findus h1",
+  "data-aos-anchor-placement":"top-top",
+})
